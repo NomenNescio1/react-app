6abff44d528475c7586d28cebd2d1f8ebd63bf41
NomenNescio1 <u1201416@unimilitar.edu.co>
2019-10-02 20:06:19 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Mi primera aplicación</h1>
+        <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
